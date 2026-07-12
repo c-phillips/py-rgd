c6a9d090f0ba7ffe1c47a74647dd237d7ca6b937
Collin Phillips <collinp1024@gmail.com>
2026-07-12 13:39:56 -0400

    Networkx dumps and tests

--- src/rgd/networkx.py
+++ src/rgd/networkx.py
@@ -1,16 +1,48 @@
+import warnings
+
 try:
     import networkx as nx
 except:
     raise ImportError("Could not import networkx")
 
-from rgd.graph import Edge, Hyperedge
+from rgd.graph import Graph, Node, Edge, Hyperedge
 from rgd.parser import loads
+from rgd.writer import dumps
+
+
+
+def _convert_networkx_graph(graph: nx.Graph) -> Graph:
+    nodes = []
+    for n, props in graph.nodes(data=True):
+        if isinstance(props, dict):
+            if len(props) == 1 and 'attributes' in props:
+                props = props['attributes']
+        nodes.append(Node(n,props))
+
+    edges = []
+    direction = Edge.Direction.DIRECTED if isinstance(graph, nx.DiGraph) else Edge.Direction.UNDIRECTED
+    for u,v,props in graph.edges(data=True):
+        if isinstance(props, dict):
+            if len(props) == 1 and 'attributes' in props:
+                props = props['attributes']
+        edges.append(Edge(u, v, direction, props))
+    return Graph(nodes, edges, {})
+
+
+def networkx_dumps(
+    graphs: nx.Graph | list[nx.Graph],
+) -> str:
+    if isinstance(graphs, (nx.Graph, nx.DiGraph)):
+        graphs = [graphs]
+    rgd_graphs = list(map(_convert_networkx_graph, graphs))
+    return dumps(rgd_graphs)
+
 
 def networkx_loads(
-    input: str,
+    source: str,
     validate: bool = True,
 ) -> nx.Graph | list[nx.Graph]:
-    basic_graphs = loads(input, validate)
+    basic_graphs = loads(source, validate)
 
     # Networkx has no hypergraph features.
     #   Raise error if any graphs include hyperedges.
@@ -22,11 +54,22 @@ def networkx_loads(
 
     nx_graphs = []
     for g in basic_graphs:
+
+        # TODO: Figure out if we want to support a secondary output containing
+        #       these properties, e.g. 
+        #       `graphs, props = rgd.networkx_loads(..., retain_props=True)`
+        if g.props:
+            warnings.warn("""\
+            Loaded RGD graph contains graph-level properties;\
+            these are not supported by NetworkX, so these properties will be dropped!\
+            """)
+
         nx_type = nx.Graph
         for edge in g.edges:
             if edge.direction != Edge.Direction.UNDIRECTED:
                 nx_type = nx.DiGraph
         ng = nx_type()
+
         for node in g.nodes:
             props = node.props if node.props is not None else dict()
             if not isinstance(node.props, dict):
